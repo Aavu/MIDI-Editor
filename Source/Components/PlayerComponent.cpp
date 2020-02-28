d9dc7e5d4a9a60054ed3a464fae53dfabbe0a00d
Kaushal Sali <salikaushal@gmail.com>
2020-02-27 22:26:26 -0500

    PlayerComponent class defined. It uses Timer class for sequencing midi messages. As of now it can take a MidiMessageSequence and print noteOn and noteOff events based on the timer.

--- Source/Components/PlayerComponent.cpp
+++ Source/Components/PlayerComponent.cpp
@@ -27,3 +27,80 @@ void PlayerComponent::paint (Graphics& g)
 void PlayerComponent::resized()
 {
 }
+
+void PlayerComponent::resetStartTime() {
+    startTime = Time::getMillisecondCounterHiRes() * 0.001;
+}
+
+void PlayerComponent::startPlayerTimer() {
+    if (midiMessageSequence)
+        startTimer(1);
+    else
+        ; //TODO: Throw uninit exception
+}
+
+void PlayerComponent::stopPlayerTimer() {
+    stopTimer();
+}
+
+void PlayerComponent::addMessageToBuffer(const MidiMessage& message) {
+    auto msgSampleNumber = message.getTimeStamp() * sampleRate; // Seconds to samples
+    //std::cout << message.getTimeStamp() << "\t" << msgSampleNumber << "\t" << message.getDescription() << std::endl;
+    buffer.addEvent(message, msgSampleNumber);
+}
+
+void PlayerComponent::addAllSequenceMessagesToBuffer() {
+    auto numEvents = midiMessageSequence->getNumEvents();
+    MidiMessageSequence::MidiEventHolder* const * eventHolder = midiMessageSequence->begin();
+    MidiMessage msg;
+    for (int i=0; i<numEvents; i++) {
+        msg = eventHolder[i]->message;
+        if (msg.isNoteOnOrOff()) {
+            addMessageToBuffer(msg);
+        }
+    }
+}
+
+void PlayerComponent::setMidiMessageSequence(const MidiMessageSequence* midiMsgSeq) {
+    midiMessageSequence = midiMsgSeq;
+    addAllSequenceMessagesToBuffer();
+}
+
+void PlayerComponent::timerCallback() {
+    auto currentTime = Time::getMillisecondCounterHiRes() * 0.001 - startTime;
+    auto currentSampleNumber = currentTime * sampleRate;
+
+    MidiBuffer::Iterator iterator(buffer);
+    MidiMessage msg;
+    int sampleNumber;
+
+    while (iterator.getNextEvent(msg, sampleNumber))
+    {
+        if (sampleNumber > currentSampleNumber)
+            break;
+        if (msg.isNoteOnOrOff())
+            std::cout <<  sampleNumber << "---" <<  msg.getTimeStamp() << "---" << msg.getDescription() << std::endl;
+    }
+    buffer.clear(previousSampleNumber, (int)currentSampleNumber - previousSampleNumber);
+
+    if (buffer.getNumEvents() <= 0) {
+        std::cout << "Finished Playing." <<  std::endl;
+        stopPlayerTimer();
+    }
+
+    previousSampleNumber = currentSampleNumber;
+}
+
+void PlayerComponent::resetPlayHead() {
+    stopPlayerTimer();
+    previousSampleNumber = 0;
+    buffer.clear();
+    addAllSequenceMessagesToBuffer();
+}
+
+void PlayerComponent::play() {
+    resetPlayHead();
+    resetStartTime();
+    startPlayerTimer();
+}
+
--- Source/Components/PlayerComponent.h
+++ Source/Components/PlayerComponent.h
@@ -15,15 +15,40 @@
 //==============================================================================
 /*
 */
-class PlayerComponent    : public Component
+
+//TODO: Implement play pause stop states.
+//TODO: Use separate thread for timing instead of Timer class.?
+
+class PlayerComponent    : public Component, private Timer
 {
 public:
     PlayerComponent();
-    ~PlayerComponent();
+    ~PlayerComponent() override;
 
     void paint (Graphics&) override;
     void resized() override;
 
+
+    void setMidiMessageSequence(const MidiMessageSequence* midiMsgSeq);
+    void resetPlayHead();
+    void play(); //This currently restarts playback.
+    //TODO: pause, stop
+
 private:
+    void timerCallback() override;
+    void addMessageToBuffer(const MidiMessage& message);
+    void addAllSequenceMessagesToBuffer();
+    void resetStartTime();
+    void startPlayerTimer();
+    void stopPlayerTimer();
+
+
+
+    const MidiMessageSequence* midiMessageSequence = nullptr;
+    MidiBuffer buffer;
+    double startTime = 0;
+    double sampleRate = 44100; //TODO: Set through constructor / setter?
+    double previousSampleNumber = 0;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlayerComponent)
 };
