a53662d0fe163a64ddd3ffd33be2e09c9d7da04b
Kaushal Sali <salikaushal@gmail.com>
2020-02-27 19:00:18 -0500

    MainComponent code from old menubar branch copied. Globals.h added to Components Group.

---
+++ Source/Components/Globals.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    Globals.h
+    Created: 12 Jan 2020 12:27:52pm
+    Author:  Raghavasimhan Sankaranarayanan
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#ifndef GLOBALS
+#define GLOBALS
+//namespace Globals {
+//MidiFile midiFile;
+//};
+#endif
