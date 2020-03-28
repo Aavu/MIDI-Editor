526a5e0ab66ebe00776c39d1d9c1b5a8b6cdc529
Kaushal Sali <salikaushal@gmail.com>
2020-03-28 18:02:44 -0400

    New MainComponentK added to avoid conflicts and maintain independent workflow.

---
+++ Source/MainComponentK.h
@@ -0,0 +1,49 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+#include "Synth/MidiSynth.h"
+#include "Synth/SfzMidiSynth.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponentK   : public AudioAppComponent,
+                         private Timer
+{
+public:
+    //==============================================================================
+    MainComponentK();
+    ~MainComponentK() override;
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (Graphics& g) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    static String getAbsolutePathOfProject(const String& projectFolderName = "MIDI-Editor");
+
+    SfzSynthAudioSource synthAudioSource;
+    MidiKeyboardState keyboardState;
+    MidiKeyboardComponent keyboardComponent;
+
+    void timerCallback() override;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponentK)
+};
